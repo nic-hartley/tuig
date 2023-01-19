9ffcd1381af98471395f145a1db356eed6bb63e0
Nic Hartley <the@redfennec.dev>
2023-01-19 00:17:45 -0500

    tiny script to run coverage so I can run it at home too

---
+++ run-coverage.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$CI" = "true" ]; then
+    output="--html"
+else
+    output="--open"
+fi
+cargo +nightly llvm-cov test \
+    --all-features --fail-under-lines 90 \
+    --ignore-filename-regex 'bin/*.rs' \
+    $output
