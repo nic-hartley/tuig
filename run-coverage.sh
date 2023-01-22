855afee1953a4b222c4ecaf00ebd1d0fd1dedcb4
Nic Hartley <the@redfennec.dev>
2023-01-21 20:06:32 -0500

    fix: specify argument once

--- run-coverage.sh
+++ run-coverage.sh
@@ -7,6 +7,5 @@ else
 fi
 cargo +nightly llvm-cov test \
     --all-features --fail-under-lines 90 \
-    --ignore-filename-regex 'bin/*|cutscenes/*' \
-    --ignore-filename-regex 'app/cli\.rs' \
+    --ignore-filename-regex 'bin/*|cutscenes/*|app/cli\.rs' \
     $output
