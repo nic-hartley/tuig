8d9e563d4d71dc22a5c88ceab1ac96a45f8883b8
Nic Hartley <the@redfennec.dev>
2023-04-09 15:46:30 -0400

    add first docs sketching some of this out

--- tuig-iosys/src/ui/mod.rs
+++ tuig-iosys/src/ui/mod.rs
@@ -1,4 +1,18 @@
-//! Eventually this module will have a nice, pleasant UI system. For now it has the legacy stuff. Have fun!
+//! The text UI system.
+//!
+//! It's based on the idea of splitting regions and filling them. You start with a [`Region`] occupying the whole
+//! screen, [`.split`](Region::split) it however you want, and then [`.attach`](Region::attach) something to the
+//! subregions. It's an immediate mode API, meaning you don't build and maintain persistent UI trees. You rebuild it
+//! every time with simple, direct method calls, imperatively divvying up and assigning the screen space.
+//!
+//! That assigned screen space is used for both input and output. Driving this system can be challenging, but at its
+//! most basic level will look something like:
+//!
+//! TODO: figure out what this looks like by implementing it in tuig
+//!
+//! You can implement your own custom attachments through the [`Attachment`] trait. The simplest ways are through the
+//! existing impls for various function types, but you might find custom impls useful for higher-level abstractions.
+//! Attachments take a `Region` and can do anything with it that you can do directly, including attach more things.
 
 #![cfg(feature = "ui")]
 
