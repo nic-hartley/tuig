bd663e7a17324e3c329b1ef9192354fad7416b28
Nic Hartley <the@redfennec.dev>
2023-04-29 02:07:58 -0400

    skeleton of new elements

--- tuig-iosys/src/ui/elements/mod.rs
+++ tuig-iosys/src/ui/elements/mod.rs
@@ -3,3 +3,7 @@
 //! Many of these also have dedicated convenience methods on [`Region`][super::Region], which are generally preferred
 //! to using the types directly. That said, all those convenience methods do is call `Region::attach` on an object in
 //! this module.
+
+mod button;
+mod textbox;
+mod text_input;
