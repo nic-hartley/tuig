7ed3b4628fa7fb96ea44c73dfac445e1797e7738
Nic Hartley <the@redfennec.dev>
2023-10-28 22:23:43 -0400

    script to automate the build/publish stuff

---
+++ x.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+CRATES="tuig-pm tuig-iosys tuig-ui tuig"
+
+mode="$1"
+
+set -e
+
+top="$(git rev-parse --show-toplevel)"
+cd "$top"
+for crate in $CRATES; do
+    if ! [ -d "$crate" ]; then
+        echo "Can't find $crate -- wrong dir? crates need updating?"
+        exit 1
+    fi
+done
+case "$mode" in
+    pub*) cargo fmt --check ;;
+    *) cargo fmt ;;
+esac
+cargo build --all-features --workspace --all-targets
+cargo doc --all-features --workspace --no-deps --lib --bins --examples
+cargo test --all-features --workspace --all-targets
+
+case "$mode" in
+    pub*)
+        for crate in $CRATES; do
+            cargo publish -p "$crate"
+        done
+        ;;
+    *)
+        for crate in $CRATES; do
+            cargo publish -p "$crate" --dry-run
+        done
+        ;;
+esac
